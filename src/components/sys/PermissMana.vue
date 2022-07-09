e5418657fd0153019551fef601c5f0cf65792bd6
Tiffany <xixueguiqishi_1@163.com>
2022-07-09 21:05:22 +0800

    权限组 提交

--- src/components/sys/PermissMana.vue
+++ src/components/sys/PermissMana.vue
@@ -4,28 +4,29 @@
       <el-input size="small" placeholder="请输入英文名" v-model="role.name">
         <template slot="prepend">ROLE_</template>
       </el-input>
-      <el-input size="small" v-model="role.nameZh" placeholder="请输入角色中文名"></el-input>
-      <el-button size="small" type="primary" icon="el-icon-plus">添加角色</el-button>
+      <el-input size="small" v-model="role.nameZh" placeholder="请输入角色中文名" @keydown.enter.n.native="doAddRole"></el-input>
+      <el-button size="small" type="primary" icon="el-icon-plus" @click="doAddRole">添加角色</el-button>
     </div>
     <div class="permissManaMain">
-      <el-collapse accordion @change="change">
+      <el-collapse v-model="activeName" accordion @change="change">
         <el-collapse-item :title="r.nameZh" :name="r.id" v-for="(r,index) in roles" :key="index">
           <el-card class="box-card">
             <div slot="header" class="clearfix">
               <span>可访问资源</span>
-              <el-button style="float: right; padding: 3px 0;color: #ff0000" type="text" icon="el-icon-delete"></el-button>
+              <el-button style="float: right; padding: 3px 0;color: #ff0000" type="text" icon="el-icon-delete" @click="doDeleteRole(r)"></el-button>
             </div>
             <div>
               <el-tree
                   :data="allMenus"
                   :props="defaultProps"
                   ref="tree"
+                  :key="index"
                   show-checkbox
                   :default-checked-keys="selectMenus"
                   node-key="id">
               </el-tree>
               <div style="display: flex;justify-content: flex-end">
-                <el-button size="mini">取消修改</el-button>
+                <el-button size="mini" @click="cancelUpdate">取消修改</el-button>
                 <el-button type="primary" size="mini" @click="doUpdate(r.id,index)" >确认修改</el-button>
               </div>
 
@@ -47,7 +48,7 @@ export default {
         name: '',
         nameZh: ''
       },
-      // activeName: '2',
+      activeName: '-1',
       roles: [],
       allMenus: [],
       defaultProps: {
@@ -61,9 +62,56 @@ export default {
     this.initRoles()
   },
   methods:{
+    doDeleteRole(role){
+      this.$confirm('此操作将永久删除【'+role.nameZh+'】角色，是否继续？','提示',{
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(()=>{
+        this.deleteRequest('/system/basic/permiss/role/'+role.id).then(resp=>{
+          if(resp){
+            this.initRoles();
+          }
+        })
+      }).catch(()=>{
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      })
+
+    },
+    doAddRole(){
+      if(this.role.name && this.role.nameZh){
+        this.postRequest('/system/basic/permiss/role',this.role).then(resp=>{
+          if(resp){
+            this.initRoles();
+            this.role.name = '';
+            this.role.nameZh = '';
+          }
+        })
+      }else{
+        this.$message.error("所有字段不能为空！")
+      }
+    },
+    cancelUpdate(){
+      this.activeName = -1;
+    },
     doUpdate(rid,index) {
+      console.log("点击确认按钮之后："+rid+","+index);
       let tree = this.$refs.tree[index];
-      console.log(tree);
+      let selectedKeys = tree.getCheckedKeys(true);
+      console.log(selectedKeys);
+      let url='/system/basic/permiss/?rid=' + rid;
+      selectedKeys.forEach(key=>{
+        url += '&mids=' + key;
+      });
+      this.putRequest(url).then(resp=>{
+        if(resp){
+          this.initRoles();
+          this.activeName = -1;
+        }
+      })
     },
     initRoles(){
       this.getRequest('/system/basic/permiss/').then(resp=>{
